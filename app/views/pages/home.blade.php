492cb4e8cdd8151180d03195b6d5b37180863da2
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-07-19 23:42:41 +0100

    home paging it up

---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+
+
+@section('content')
+  <div class="jumbotron">
+    <h1>Welcome to Larabook!</h1>
+    <p>Welcome to the premier place to talk about laravel with others. Why don't you sign up to see what all the fuss is about?</p>
+    <p>
+      <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign up!</a>
+    </p>
+  </div>
+@stop
