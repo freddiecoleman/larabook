953111056592a4a27488bac04a62970c6e33e7ea
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-07-20 11:49:31 +0100

    gulp changes

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,5 +5,12 @@ var autoprefixer = require('gulp-autoprefixer');
 gulp.task('css', function() {
   gulp.src('app/assets/sass/main.scss')
     .pipe(sass())
+    .pipe(autoprefixer('last 10 version'))
     .pipe(gulp.dest('public/css'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('app/assets/sass/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['watch']);
