9b25e631cc1dc4aa0a2cac29a2872ac74e7529d1
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-07-20 12:58:32 +0100

    sign up test

--- tests/functional/SignUpCept.php
+++ tests/functional/SignUpCept.php
@@ -6,3 +6,12 @@ $I->wantTo('sign up for a Larabook account');
 $I->amOnPage('/');
 $I->click('Sign Up');
 $I->seeCurrentUrlEquals('/register');
+
+$I->fillField('Username:', 'JohnDoe');
+$I->fillField('Email:', 'john@example.com');
+$I->fillField('Password:', 'demo');
+$I->fillField('Password Confirmation:', 'demo');
+$I->click('Sign Up');
+
+$I->seeCurrentUrlEquals('');
+$I->see('Welcome to Larabook!');
