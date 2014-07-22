346db2e670ce5d747cb16081abf7090a87b58222
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-07-22 21:28:22 +0100

    styling

--- app/views/registration/create.blade.php
+++ app/views/registration/create.blade.php
@@ -2,35 +2,39 @@
 
 
 @section('content')
-  <h1>Register!</h1>
+    <div class="row">
+        <div class="col-md-6">
+          <h1>Register!</h1>
 
-  @include('layouts.partials.errors')
+          @include('layouts.partials.errors')
 
-  {{ Form::open(['route' => 'register_path']) }}
+          {{ Form::open(['route' => 'register_path']) }}
 
-    <div class="form-group">
-      {{ Form::label('username', 'Username:') }}
-      {{ Form::text('username', null, ['class' => 'form-control']) }}
+            <div class="form-group">
+              {{ Form::label('username', 'Username:') }}
+              {{ Form::text('username', null, ['class' => 'form-control']) }}
+            </div>
+
+            <div class="form-group">
+              {{ Form::label('email', 'Email:') }}
+              {{ Form::text('email', null, ['class' => 'form-control']) }}
+            </div>
+
+            <div class="form-group">
+              {{ Form::label('password', 'Password:') }}
+              {{ Form::password('password', ['class' => 'form-control']) }}
+            </div>
+
+            <div class="form-group">
+              {{ Form::label('password_confirmation', 'Password Confirmation:') }}
+              {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
+            </div>
+
+            <div class="form-group">
+              {{ Form::submit('Sign up!', ['class' => 'btn btn-primary']) }}
+            </div>
+
+          {{ Form::close() }}
+        </div>
     </div>
-
-    <div class="form-group">
-      {{ Form::label('email', 'Email:') }}
-      {{ Form::text('email', null, ['class' => 'form-control']) }}
-    </div>
-
-    <div class="form-group">
-      {{ Form::label('password', 'Password:') }}
-      {{ Form::password('password', ['class' => 'form-control']) }}
-    </div>
-
-    <div class="form-group">
-      {{ Form::label('password_confirmation', 'Password Confirmation:') }}
-      {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
-    </div>
-
-    <div class="form-group">
-      {{ Form::submit('Sign up!', ['class' => 'btn btn-primary']) }}
-    </div>
-
-  {{ Form::close() }}
 @stop
