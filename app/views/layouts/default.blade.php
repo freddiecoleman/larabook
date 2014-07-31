2d5843e50404614d965c8086df5607ebe1b8dc92
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-07-31 21:25:09 +0100

    add flash message

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -7,10 +7,17 @@
   <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
+
   @include('layouts.partials.nav')
+
   <div class="container">
+    @include('flash::message')
+
     @yield('content')
   </div>
+
+  <script src="//code.jquery.com/jquery.js"></script>
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
 </body>
 </html>
