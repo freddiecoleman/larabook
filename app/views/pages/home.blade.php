8bad8309514229fd88ecbc9db1f3c782df6038b8
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-07-31 21:51:22 +0100

    check if user logged in

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -5,8 +5,10 @@
   <div class="jumbotron">
     <h1>Welcome to Larabook!</h1>
     <p>Welcome to the premier place to talk about laravel with others. Why don't you sign up to see what all the fuss is about?</p>
-    <p>
-      {{ link_to_route('register_path', 'Sign up!', null, ['class' => 'btn btn-lg btn-primary'])}}
-    </p>
+    @if ( ! $currentUser)
+        <p>
+          {{ link_to_route('register_path', 'Sign up!', null, ['class' => 'btn btn-lg btn-primary'])}}
+        </p>
+    @endif
   </div>
 @stop
