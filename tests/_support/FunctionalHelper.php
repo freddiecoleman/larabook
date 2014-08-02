bf7c597b6d37f91cdd60138a1a81d303d6d7a1de
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-02 10:37:45 +0100

    testing

--- tests/_support/FunctionalHelper.php
+++ tests/_support/FunctionalHelper.php
@@ -1,5 +1,6 @@
-<?php
-namespace Codeception\Module;
+<?php namespace Codeception\Module;
+
+use Laracasts\TestDummy\Factory as TestDummy;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
@@ -8,7 +9,7 @@ class FunctionalHelper extends \Codeception\Module {
 
     public function haveAnAccount()
     {
-
+        $user = TestDummy::create('Larabook\Users\User');
     }
-    
+
 }
