949b1b101c99134cfc4d971ede265b1d9330546f
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-02 11:23:40 +0100

    login form

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
     <h1>Sign In!</h1>
+
+    {{ Form::open() }}
+        <div class="form-group">
+            {{ Form::label('email', 'Email:') }}
+            {{ Form::email('email', null, ['class' => 'form-control', 'required' => 'required']) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::label('password', 'Password:') }}
+            {{ Form::password('password', ['class' => 'form-control', 'required' => 'required']) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::submit('Sign In', ['class' => 'btn btn-primary']) }}
+        </div>
+    {{ Form::close() }}
 @stop
