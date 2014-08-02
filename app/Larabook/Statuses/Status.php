4470e93c35f053764e1271e479b58a4606e2b95c
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-02 19:56:32 +0100

    Create statuses model

---
+++ app/Larabook/Statuses/Status.php
@@ -0,0 +1,11 @@
+<?php namespace Larabook\Statuses;
+
+class Status extends \Eloquent {
+
+    /**
+     * Fillable fields for a new status.
+     * @var array
+     */
+    protected $fillable = ['body'];
+
+}
