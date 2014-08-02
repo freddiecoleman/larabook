c2a16243fa7c28338397b8eff63ea724acd0632a
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-02 10:55:00 +0100

    create login page

--- app/routes.php
+++ app/routes.php
@@ -15,16 +15,24 @@
 Registration
 */
 Route::get('/', [
-  'as' => 'home',
-  'uses' => 'PagesController@home'
+    'as'   => 'home',
+    'uses' => 'PagesController@home'
 ]);
 
 Route::get('register', [
-  'as' => 'register_path',
-  'uses' => 'RegistrationController@create'
+    'as'   => 'register_path',
+    'uses' => 'RegistrationController@create'
 ]);
 
 Route::post('register', [
-  'as' => 'register_path',
-  'uses' => 'RegistrationController@store'
+    'as'   => 'register_path',
+    'uses' => 'RegistrationController@store'
+]);
+
+/**
+ *  Sessions
+ */
+Route::get('login', [
+    'as'   => 'login_path',
+    'uses' => 'SessionsController@create'
 ]);
