dca38274ac9f2e4f4ffdad94f732d1bfddcc24b5
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-03 18:11:15 +0100

    styling

--- app/views/statuses/index.blade.php
+++ app/views/statuses/index.blade.php
@@ -1,25 +1,33 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Post a status</h1>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
 
-    @include('layouts.partials.errors')
+            @include('layouts.partials.errors')
 
-    {{ Form::open() }}
-        <div class="form-group">
-            {{ Form::label('body', 'Status:') }}
-            {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+            {{ Form::open() }}
+                <div class="form-group">
+                    {{ Form::label('body', 'Status:') }}
+                    {{ Form::textarea('body', null, ['class' => 'form-control', 'placeholder' => 'What\'s on your mind?']) }}
+                </div>
+                <div class="form-group">
+                    {{ Form::submit('Post Status', ['class' => 'btn btn-primary']) }}
+                </div>
+            {{ Form::close() }}
+
+            @foreach($statuses as $status)
+                <article class="media status-media">
+                    <div class="pull-left">
+                        <img class="media-object" src="{{ $status->user->present()->gravatar }}" alt="{{ $status->user->username }}">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">{{ $status->user->username }}</h4>
+                        <p>{{ $status->created_at->diffForHumans() }}</p>
+                        {{ $status->body }}
+                    </div>
+                </article>
+            @endforeach
         </div>
-        <div class="form-group">
-            {{ Form::submit('Post Status', ['class' => 'btn btn-primary']) }}
-        </div>
-    {{ Form::close() }}
-
-    <h2>Statuses</h2>
-
-    @foreach($statuses as $status)
-        <article>
-            {{ $status->body }}
-        </article>
-    @endforeach
+    </div>
 @stop
