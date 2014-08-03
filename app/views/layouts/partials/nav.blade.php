eaf370208367a07d71d00dc8b2430da55d13411d
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-03 15:18:22 +0100

    styling gravatar

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -18,7 +18,12 @@
       <ul class="nav navbar-nav navbar-right">
         @if ($currentUser)
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ $currentUser->username }} <span class="caret"></span></a>
+
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <img src="//www.gravatar.com/avatar/{{ md5($currentUser->email) }}?s=30" alt="{{ $currentUser->username }}" class="nav-gravatar">
+                {{ $currentUser->username }} <span class="caret"></span>
+              </a>
+
               <ul class="dropdown-menu" role="menu">
                 <li><a href="#">Action</a></li>
                 <li><a href="#">Another action</a></li>
@@ -26,7 +31,7 @@
                 <li class="divider"></li>
                 <li><a href="#">Separated link</a></li>
                 <li class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
+                <li>{{ link_to_route('logout_path', 'Logout') }}</li>
               </ul>
             </li>
         @else
