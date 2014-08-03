a79dbad3d41349b4d582382423dc48f3228da817
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-03 18:29:32 +0100

    tidying up statuses with a partial

--- app/views/statuses/index.blade.php
+++ app/views/statuses/index.blade.php
@@ -18,16 +18,7 @@
             </div>
 
             @foreach($statuses as $status)
-                <article class="media status-media">
-                    <div class="pull-left">
-                        <img class="media-object" src="{{ $status->user->present()->gravatar }}" alt="{{ $status->user->username }}">
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">{{ $status->user->username }}</h4>
-                        <p>{{ $status->created_at->diffForHumans() }}</p>
-                        {{ $status->body }}
-                    </div>
-                </article>
+                @include('statuses.partials.status')
             @endforeach
         </div>
     </div>
