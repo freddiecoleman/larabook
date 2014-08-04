ddb3413e282308e9ee83fa276e1bfaa523c56f7c
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-04 20:22:00 +0100

    updating seeder

--- app/database/seeds/StatusesTableSeeder.php
+++ app/database/seeds/StatusesTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-// Composer: "fzaninotto/faker": "v1.3.0"
 use Faker\Factory as Faker;
+use Larabook\Statuses\Status;
 
 class StatusesTableSeeder extends Seeder {
 
@@ -9,7 +9,7 @@ class StatusesTableSeeder extends Seeder {
 	{
 		$faker = Faker::create();
 
-		foreach(range(1, 10) as $index)
+		foreach(range(1, 50) as $index)
 		{
 			Status::create([
 
