dfcdf5f236c7c83f74757f450cfc124a9f6bb2d5
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-06 20:14:52 +0100

    display user avatars and refactor code to support this

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -5,7 +5,10 @@
         <div class="col-md-6 col-md-offset-3">
             <h1>All Users</h1>
             @foreach($users as $user)
-                <li>{{ $user->username }}</li>
+                <div class="col-md-3">
+                    @include('layouts.partials.avatar')
+                    {{ $user->username }}
+                </div>
             @endforeach
         </div>
     </div>
