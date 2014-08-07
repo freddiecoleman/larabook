12643be81fa4c11fafd693e689dce3eb3fd5e342
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-07 21:34:48 +0100

    test fetching statuses  by username :D

--- tests/_support/FunctionalHelper.php
+++ tests/_support/FunctionalHelper.php
@@ -10,9 +10,10 @@ class FunctionalHelper extends \Codeception\Module {
     public function signIn()
     {
         $email = 'foo@example.com';
+        $username = 'foobar';
         $password = 'foo';
 
-        $this->haveAnAccount(compact('email', 'password'));
+        $this->haveAnAccount(compact('email', 'username', 'password'));
 
         $I = $this->getModule('Laravel4');
 
---
+++ tests/functional/ProfileCept.php
@@ -0,0 +1,14 @@
+<?php
+
+$I = new FunctionalTester($scenario);
+$I->am('a Larabook member');
+$I->wantTo('view my profile');
+
+$I->signIn();
+
+$I->postAStatus('My new status.');
+
+$I->click('Your Profile');
+$I->seeCurrentUrlEquals('/@foobar');
+
+$I->see('My new status.');
