5d03a293696e1e665b433e8ddbfb8bd3b2f3eab9
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-07 21:00:12 +0100

    sort statuses

--- app/Larabook/Users/UserRepository.php
+++ app/Larabook/Users/UserRepository.php
@@ -30,7 +30,10 @@ class UserRepository {
      */
     public function findByUsername($username)
     {
-        return User::whereUsername($username)->first();
+        return User::with(['statuses' => function($query)
+        {
+            $query->latest();
+        }])->whereUsername($username)->first();
     }
 
 } 
