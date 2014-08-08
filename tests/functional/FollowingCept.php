91ee33ea4dddaeea4829c1859257f102b8b6d9d9
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-08 21:17:41 +0100

    functional test for following a user

---
+++ tests/functional/FollowingCept.php
@@ -0,0 +1,18 @@
+<?php 
+$I = new FunctionalTester($scenario);
+
+$I->am('a Larabook member');
+$I->wantTo('follow other Larabook users');
+
+$I->haveAnAccount('Larabook\Users\User', ['username' => 'OtherUser'])
+
+$I->click('Browse Users');
+$I->click('OtherUser');
+
+$I->seeCurrentUrlEquals('/@OtherUser');
+$I->click('Follow OtherUser');
+$I->seeCurrentUrlEquals('/@OtherUser');
+
+$I->see('You are following OtherUser.');
+$I->dontSee('Follow OtherUser');
+
