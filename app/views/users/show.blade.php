1725e1dd674897e36bc869f731d0f25fc81b8dca
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-09 10:57:29 +0100

    display status count

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -2,13 +2,20 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-3">
-            <h1>{{ $user->username }}</h1>
-            @include('users.partials.avatar', ['size' => 100])
+        <div class="col-md-4">
+            <div class="media">
+                <div class="pull-left">
+                    @include('users.partials.avatar', ['size' => 50])
+                </div>
 
-            @unless ($user->is($currentUser))
-                @include('users.partials.follow-form')
-            @endif
+                <div class="media-body">
+                    <h1 class="media-heading">{{ $user->username }}</h1>
+                    <p class="text-muted">{{ $status_count = $user->statuses->count() }} {{ str_plural('Status', $status_count) }}</p>
+                    @unless ($user->is($currentUser))
+                        @include('users.partials.follow-form')
+                    @endif
+                </div>
+            </div>
         </div>
         <div class="col-md-6">
             @if ($user->is($currentUser))
