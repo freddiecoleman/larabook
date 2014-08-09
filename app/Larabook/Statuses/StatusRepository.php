c5484aa11222f485fe148e8f2c164600b7ac6273
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-09 12:11:51 +0100

    brought tests up to date

--- app/Larabook/Statuses/StatusRepository.php
+++ app/Larabook/Statuses/StatusRepository.php
@@ -30,7 +30,7 @@ class StatusRepository {
      */
     public function getFeedForUser(User $user)
     {
-        $userIds = $user->follows()->lists('followed_id');
+        $userIds = $user->followedUsers()->lists('followed_id');
         $userIds[] = $user->id;
 
         return Status::whereIn('user_id', $userIds)->latest()->get();
