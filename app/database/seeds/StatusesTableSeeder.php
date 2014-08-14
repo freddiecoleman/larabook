58add1b16fa644b34898487b2a6bd1ae061337b0
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-14 19:17:46 +0100

    change pagination style, add random dates to seeder

--- app/database/seeds/StatusesTableSeeder.php
+++ app/database/seeds/StatusesTableSeeder.php
@@ -6,19 +6,20 @@ use Larabook\Users\User;
 
 class StatusesTableSeeder extends Seeder {
 
-	public function run()
-	{
-		$faker = Faker::create();
+    public function run()
+    {
+        $faker = Faker::create();
 
         $userIds = User::lists('id');
 
-		foreach(range(1, 1000) as $index)
-		{
-			Status::create([
-                'user_id' => $faker->randomElement($userIds),
-                'body'    => $faker->sentence()
+        foreach (range(1, 1000) as $index)
+        {
+            Status::create([
+                'user_id'    => $faker->randomElement($userIds),
+                'body'       => $faker->sentence(),
+                'created_at' => $faker->dateTime()
             ]);
-		}
-	}
+        }
+    }
 
 }
